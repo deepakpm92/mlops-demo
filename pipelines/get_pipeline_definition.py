3a181719e05aec33bb4454d6c81f68d27c5cbd1e
Deepak Menon <deepakmenon355@gmail.com>
2024-12-02 23:48:11 +0530

    Update get_pipeline_definition.py

--- pipelines/get_pipeline_definition.py
+++ pipelines/get_pipeline_definition.py
@@ -10,7 +10,7 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-"""A CLI to get pipeline definitions from pipeline modules."""
+"""A CLI to get pipeline definitions from pipeline modules"""
 from __future__ import absolute_import
 
 import argparse
